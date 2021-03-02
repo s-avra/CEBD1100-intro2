4c4da1951451373f7f736084401dea3c5c7d3891
Sara Avramovic <sarakavramovic@gmail.com>
2021-03-01 23:01:53 -0500

    assignment 1 q1 save 2

--- Assignment1/assignment1_question1.py
+++ Assignment1/assignment1_question1.py
@@ -1,28 +1,42 @@
 # main menu
+goodbye_menu2 = "Thanks for drawing triangles! Goodbye."
 print("Welcome!")
 while True:
     main_menu = "Please select an option below:\n1 - Draw a Triangle\nQ - Quit"
     print(main_menu)
-    main_choice = input("Which option would you like? ").strip().upper()
-    if main_choice != "1" and "Q":
-        print("Sorry!\nThat option is not available, please enter one of the listed options.")
+    main_choice = input("Which option would you like? ").upper()
     if main_choice == "1":
-        print("Good choice!")
+        print("\nGood choice!")
         # menu 2
         while True:
-            menu2 = "What kind of triangle would you like to draw?\n1 - Right sided triangle\n2 - Isosceles triangle\nQ - Back to main menu"
+            menu2 = "\nChoose which triangle you would like to draw:\n1 - Right sided triangle\n2 - Isosceles triangle\nQ - Back to main menu"
             print(menu2)
             menu2_choice = input("Which option would you like? ").upper()
-            if menu2_choice == "Q":
-                break
             if menu2_choice == "1":
-                print("What size would you like your right sided triangle?")
-                break
+                right_size = int(input("\nWhat size would you like your right sided triangle? "))
+                for x in range(1, right_size+1):
+                    print("#"*x)
+                try_again =input("I hope you like your triangle! Press \"A\" if you would like to try again.\nPress anything else to go back to the main menu. ").upper().strip()
+                if try_again == "A":
+                    continue
+                else:
+                    print()
+                    break
             if menu2_choice == "2":
-                print("What size would you like your isosceles triangle?")
+                iso_size=int(input("\nWhat size would you like your isosceles triangle? "))
+                for z in range(1,iso_size+1,2):
+                    print(int(((iso_size-z)/2))*" ", end = "")
+                    print(z*"#", end = "")
+                    print(int(((iso_size - z) / 2)) * " ")
+                break
+            if menu2_choice == "Q":
+                print()
                 break
             else:
                 print("Sorry!\nThat option is not available, please enter one of the listed options.")
+        continue
     if main_choice == "Q":
         print("Goodbye!")
         break
+    else:
+        print("Sorry, try again")
