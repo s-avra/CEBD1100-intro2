0cae527c0ac589dcff06a7caba1a20e99f4ba595
Sara Avramovic <sarakavramovic@gmail.com>
2021-03-02 22:09:17 -0500

    assignment 1 q2 save 2

--- Assignment1/assignment1_question2.py
+++ Assignment1/assignment1_question2.py
@@ -6,10 +6,14 @@ def is_an_int(num):
         return False
 table_size = input("What size multiplication table would you like?")
 
-print((7+int(int(table_size)/2))*" " + "Multiplication Table"+int(int(table_size)/2)*" ")
-print()
+print((int((int(table_size)+4)/2))*" " + "Multiplication Table"+int((int(table_size)+4)/2)*" ")
 table_int=int(table_size)
-print(7*" ",end= "")
+print((4)*" ",end= "")
 for a in range(1,table_int+1):
-    print("{0:3}".format(a), end = "")
-    print
+    print("{0:2}  ".format(a), end="")
+print("\n"+"_"*(4+(table_int*4)), end="")
+for b in range(1, table_int+1):
+    print("\n")
+    print(str(b) + " |  ", end="")
+    for c in range(1,table_int+1):
+        print("{0:4}".format(str(b*c)), end="")
