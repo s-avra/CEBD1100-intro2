674246a03293affd6260a4ad594eb33ec8e4343a
Sara Avramovic <sarakavramovic@gmail.com>
2021-03-03 00:28:42 -0500

    assignment 1 q3 save 1

---
+++ Assignment1/assignment1_question3.py
@@ -0,0 +1,13 @@
+#variables
+num_range = 25
+fizz_multiplier = 3
+buzz_multiplier = 5
+# list execution
+for x in range(0,num_range+1):
+    print("{0:2} :".format(x), end="")
+    if x % fizz_multiplier ==0 and x!= 0:
+        print("Fizz", end="")
+    if x % buzz_multiplier ==0 and x!= 0:
+        print("Buzz", end="")
+    print()
+
