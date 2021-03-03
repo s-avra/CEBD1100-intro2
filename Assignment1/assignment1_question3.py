4a6e0ff7850310840683c8eec342b385583c7ea9
Sara Avramovic <sarakavramovic@gmail.com>
2021-03-03 00:32:11 -0500

    assignment 1 q3 save 2

--- Assignment1/assignment1_question3.py
+++ Assignment1/assignment1_question3.py
@@ -2,9 +2,9 @@
 num_range = 25
 fizz_multiplier = 3
 buzz_multiplier = 5
-# list execution
+# FizzBuzz execution
 for x in range(0,num_range+1):
-    print("{0:2} :".format(x), end="")
+    print("{0:2}: ".format(x), end="")
     if x % fizz_multiplier ==0 and x!= 0:
         print("Fizz", end="")
     if x % buzz_multiplier ==0 and x!= 0:
